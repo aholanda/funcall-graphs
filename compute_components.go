c62e6c3bf4ee3b03acf79cce56c7436e843cc80c
Adriano Holanda <aholanda@usp.br>
2020-12-23 11:09:16 -0300

    prepare output for processed data

--- compute_components.go
+++ compute_components.go
@@ -60,17 +60,36 @@ func listDataFiles() []string {
 
 func computeComponents() {
 	var digraph *g.Digraph
+	var sep string = ";"
+
+	file, err := os.Open("scc.dat")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fmt.Fprintf(file, "#vertices%s", sep)
+	fmt.Fprintf(file, "#arcs%s", sep)
+	fmt.Fprintf(file, "avg_degree: %s", sep)
+	fmt.Fprintf(file, "std_dev: %s", sep)
+	fmt.Fprintf(file, "#components: %s", sep)
+	fmt.Fprintf(file, "greatest_comp_size: %s", sep)
+	fmt.Fprintf(file, "\n")
 
 	files := listDataFiles()
-
 	for _, f := range files {
 		log.Printf("> reading %v\n", f)
 		digraph = gio.ReadPajek(f)
 		scc := g.NewKosarajuSharirSCC(digraph)
 		scc.Compute()
-		fmt.Printf("#vertices: %d\n", digraph.V())
-		fmt.Printf("#arcs: %d\n", digraph.A())
-		fmt.Printf("#components: %d\n", scc.Count())
+		fmt.Fprintf(file, "%d%s", digraph.V(), sep)
+		fmt.Fprintf(file, "%d%s", digraph.A(), sep)
+		avgDeg, stdDev := digraph.AverageDegree()
+		fmt.Fprintf(file, "%f%s", avgDeg, sep)
+		fmt.Fprintf(file, "%f%s", stdDev, sep)
+		fmt.Fprintf(file, "%d%s", scc.Count(), sep)
+		fmt.Fprintf(file, "%d%s", scc.GreatestComponentSize(), sep)
+		fmt.Fprintf(file, "\n")
 		break
 	}
 }
