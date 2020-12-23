c7b2357620936678675a3759f0b545f476f0cbbe
Adriano Holanda <aholanda@usp.br>
2020-12-23 15:15:41 -0300

    extract the version numbering the write in the csv file

--- compute_components.go
+++ compute_components.go
@@ -47,7 +47,7 @@ func listDataFiles() []string {
 	var files []string
 	filepath.Walk(pathS, func(fpath string, f os.FileInfo, _ error) error {
 		if !f.IsDir() {
-			r, err := regexp.MatchString(".net", f.Name())
+			r, err := regexp.MatchString(GraphFormatExtension, f.Name())
 			if err == nil && r {
 				filepath := path.Join("data/", f.Name())
 				files = append(files, filepath)
@@ -58,30 +58,46 @@ func listDataFiles() []string {
 	return files
 }
 
+func extractVersionNumbering(path string) string {
+	exp := fmt.Sprintf(`\blinux-(\d+\.\d+\.*\d*)%s`,
+		GraphFormatExtension)
+	re := regexp.MustCompile(exp)
+	ret := re.FindStringSubmatch(path)
+
+	if len(ret) == 0 {
+		log.Fatalf("fatal: error finding version number for \"%s\"", path)
+	}
+	return ret[1]
+}
+
 func computeComponents() {
 	var digraph *g.Digraph
 	var sep string = ";"
+	var resFn = "scc.csv"
 
-	file, err := os.Open("scc.dat")
+	file, err := os.Create(resFn)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
+	defer log.Printf("> wrote file %s\n", resFn)
 
 	fmt.Fprintf(file, "#vertices%s", sep)
 	fmt.Fprintf(file, "#arcs%s", sep)
-	fmt.Fprintf(file, "avg_degree: %s", sep)
-	fmt.Fprintf(file, "std_dev: %s", sep)
-	fmt.Fprintf(file, "#components: %s", sep)
-	fmt.Fprintf(file, "greatest_comp_size: %s", sep)
+	fmt.Fprintf(file, "avg_degree%s", sep)
+	fmt.Fprintf(file, "std_dev%s", sep)
+	fmt.Fprintf(file, "#components%s", sep)
+	fmt.Fprintf(file, "greatest_comp_size%s", sep)
 	fmt.Fprintf(file, "\n")
 
-	files := listDataFiles()
-	for _, f := range files {
-		log.Printf("> reading %v\n", f)
-		digraph = gio.ReadPajek(f)
+	filenames := listDataFiles()
+	for _, fn := range filenames {
+		log.Printf("> reading %v\n", fn)
+		digraph = gio.ReadPajek(fn)
 		scc := g.NewKosarajuSharirSCC(digraph)
 		scc.Compute()
+		version := extractVersionNumbering(fn)
+		fmt.Fprintf(file, "%s%s", version, sep)
 		fmt.Fprintf(file, "%d%s", digraph.V(), sep)
 		fmt.Fprintf(file, "%d%s", digraph.A(), sep)
 		avgDeg, stdDev := digraph.AverageDegree()
@@ -90,6 +106,6 @@ func computeComponents() {
 		fmt.Fprintf(file, "%d%s", scc.Count(), sep)
 		fmt.Fprintf(file, "%d%s", scc.GreatestComponentSize(), sep)
 		fmt.Fprintf(file, "\n")
-		break
+		//break
 	}
 }
